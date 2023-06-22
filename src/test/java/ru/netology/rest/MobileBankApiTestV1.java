56c28e07a12c349937e25c544021d8ef8c37960f
AnnWolkova <volkov.nikita.a@gmai.com>
2023-06-22 17:56:35 +0700

    5 change

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,7 +1,12 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
@@ -17,5 +22,20 @@ class MobileBankApiTestV1 {
       // Проверки
       .then()
           .statusCode(200);
+    }
+    @Test
+    void shouldReturnDemoAccountsBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[1].currency", equalTo("USD"));
+
+
     }
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,5 +1,10 @@
 package ru.netology.rest;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
